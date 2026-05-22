fbfc33e9012ffb464c52b41faf5ce0b021f24010
Kevin Le <vinhdat82@gmail.com>
2026-05-22 14:42:39 +0700

    Added build for Heltec v4

--- MeshCore/build-binary-files.sh
+++ MeshCore/build-binary-files.sh
@@ -1,3 +1,5 @@
 # Packet Filter
 export FIRMWARE_VERSION="BridgeFilter-1.0"
-sh build.sh build-firmware Heltec_v3_repeater_bridge_espnow
+sh build.sh build-firmware \ 
+Heltec_v3_repeater_bridge_espnow \
+heltec_v4_tft_repeater_bridge_espnow
